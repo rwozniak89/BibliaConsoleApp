1e5195e8e3979e972b765efc62d96d5140ef38dc
Radosław Woźniak <rwozniak89@gmail.com>
2020-04-12 14:35:00 +0200

    Dodaj pliki projektów.

---
+++ BibliaConsoleApp/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BibliaConsoleApp
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string lastText = string.Empty;
+            string text = string.Empty;
+
+            Console.WriteLine("Program do konwersji tekstu na bibliografię. Wciśnij klawisz \\ po skopiowaniu tekstu, aby uruchomić parser");
+
+            while (true)
+            {
+
+                string start = Console.ReadLine();
+                if(start == "\\")
+                {
+
+                try
+                {
+                    if (Clipboard.ContainsText(TextDataFormat.Text))
+                    {
+                        text = Clipboard.GetText(TextDataFormat.Text);
+                        if (string.IsNullOrEmpty(text) || lastText.Equals(text)) { }
+                        else if (text.Length > 15)
+                        {
+                            lastText = text;
+                            Console.WriteLine(text);
+
+                            var tab = text.Split('\n');
+                            Console.WriteLine("START ---------- SPLITER ------------------");
+                            string title = "", authors = "", extra = "", article = "", year = "", volumePaper = "";
+                            if (tab != null && tab.Length == 3)
+                            {
+                                title = tab[0].Replace('\r', ' ').Trim();
+                                authors = tab[1].Replace('\r', ' ').Trim();
+                                extra = tab[2].Replace('\r', ' ').Trim();
+
+                                var names = authors.Split(';');
+                                if (names != null)
+                                {
+                                    authors = "";
+                                    int i = 0;
+                                    foreach (string name in names)
+                                    {
+                                        i++;
+                                        if (i >= 4)
+                                        {
+                                            authors = authors.Substring(0, authors.Length - 2) + " i inni, ";
+                                            break;
+                                        }
+                                                
+                                        var person = name.Split(',');
+                                        if (person != null)
+                                        {
+                                            if (person.Length == 1) authors += person;
+                                            else if (person.Length == 2)
+                                            {
+                                                authors += person[0];
+                                                var firstNames = person[1].Split('.');
+                                                if (firstNames != null)
+                                                {
+                                                    foreach (string fn in firstNames)
+                                                    {
+                                                        string fff = fn.Trim().Substring(0, 1);
+                                                        authors += " " + fff + ".";
+                                                    }
+                                                }
+                                            }
+                                            authors += ", ";
+                                        }
+                                    }
+                                }
+
+                                var extras = extra.Split(',');
+
+                                if (extras != null && extras.Length >= 2)
+                                {
+                                    int i = 0;
+                                    foreach (string e in extras)
+                                    {
+                                        i++;
+                                        if (i == 1)
+                                        {
+                                            article = "\"" + e + "\",";
+                                        }
+                                        else if (i == 2)
+                                        {
+                                            string tempYear = e.Trim();
+                                            if(tempYear.Length>4)
+                                            {
+                                                tempYear = " " + tempYear.Substring(tempYear.Length - 4);
+                                            }
+                                            year = tempYear + " r.,";
+                                        }
+                                        else
+                                        {
+                                            string ee = "";
+                                            ee += e.Replace("Vol.", "tom ").Replace("pp.", "str. ").Replace("p.", "str. ");
+                                            volumePaper += ee;
+                                            if (i == extras.Length) volumePaper += ".";
+                                            else volumePaper += " ,";
+                                        }
+                                    }
+                                }
+                            }
+
+                            string result = authors + title + ", " + article + year + volumePaper;
+
+                            result = result.Replace(" ,", ",").Replace("  ", " ");
+
+                            Clipboard.SetText(result);
+                            lastText = result;
+                            Console.WriteLine("Wynik: " + result);
+
+
+                        }
+
+
+                       
+                        
+                    }
+                    //Console.WriteLine(lastText);
+                    Thread.Sleep(1000);
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+
+                }
+            }
+            
+        }
+        
+
+
+
+        
+    }
+}
---
+++ BibliaConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("BibliaConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BibliaConsoleApp")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("22e7df7f-6940-4480-9eb8-8f8f0c11337e")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
